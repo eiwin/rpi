ab99404ff7bfbaf831058b681f61cd3abae0c4a2
eiwin <eiwin@163.com>
2020-05-17 22:57:26 -0400

    Update auto_mount_usb.py

--- auto_mount_usb.py
+++ auto_mount_usb.py
@@ -1,17 +1,19 @@
-import commands
-
+import subprocess
 # ASSUMED THAT THIS COMMAND HAS ALREADY BEEN RUN
 # sudo mkdir /mnt/usb_stick
 
-MOUNT_DIR = "/mnt/usb_stick"
+MOUNT_DIR = "/media/usb"
 
 def run_command(command):
     # start = time.time()
-    ret_code, output = commands.getstatusoutput(command)
-    if ret_code == 1:
+    try:
+        output = subprocess.check_output(command, shell=True)
+    except subprocess.CalledProcessError as ex:
+        return None
         raise Exception("FAILED: %s" % command)
     # end = time.time()
     # print "Finished in %s seconds" % (end - start)
+    print(output)
     return output.splitlines() 
 
 
@@ -22,9 +24,20 @@ def uuid_from_line(line):
     uuid_end = uuid_start + len(example_uuid)
     return line[uuid_start: uuid_end]
 
-output = run_command("blkid | grep LABEL | grep -v boot")
-# ['/dev/sda1: LABEL="KINGSTON" UUID="6784-3407" TYPE="vfat" PARTUUID="459720e1-01"']
-for usb_device in output:
-    command = "mount --uuid %s %s" % (uuid_from_line(usb_device), MOUNT_DIR)
+def mount_usb():
+    output = run_command("blkid | grep sda")
+    if None==output:
+        return
+    # ['/dev/sda1: LABEL="KINGSTON" UUID="6784-3407" TYPE="vfat" PARTUUID="459720e1-01"']
+    for usb_device in output:
+        command = "sudo mount /dev/sda1 %s" % (MOUNT_DIR)
+        run_command(command)
+        break
+
+def umount_usb():
+    command = "sudo umount %s" % (MOUNT_DIR)
     run_command(command)
-    break
+    
+mount_usb()
+
+umount_usb()    
